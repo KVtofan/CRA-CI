a056b08da4cb5b26b4af1207c80cab5cfd129de5
KVtofan <kv.tofan@gmail.com>
2020-02-24 16:32:07 +0300

    Updates

---
+++ precache-manifest.0eb720e9864687893bae12c88019d129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91f8c1e47374acc6ace31381535e075",
+    "url": "/cra-ci/index.html"
+  },
+  {
+    "revision": "ec1148ece94f6eacb8b2",
+    "url": "/cra-ci/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17f381f4864c17b5c158",
+    "url": "/cra-ci/static/js/2.006f4d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra-ci/static/js/2.006f4d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1148ece94f6eacb8b2",
+    "url": "/cra-ci/static/js/main.a9307904.chunk.js"
+  },
+  {
+    "revision": "fc20ee949f75aa7dd14b",
+    "url": "/cra-ci/static/js/runtime-main.2008f62e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
