8df480a6dbbcd26cbf6a2c6fa5f408a74feaa4d1
KVtofan <kv.tofan@gmail.com>
2020-02-24 16:44:20 +0300

    add gh pages.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test <code>Create React App</code> runned via Github actions.
+          Test <code>Create React App</code> runned via Github actions. KVtofan
         </p>
         <a
           className="App-link"
